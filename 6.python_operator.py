c6a0bef30ee0d426adcc3f6108596c4dbb744372
gawchar33 <gawchar33@gmail.com>
2026-03-03 10:51:38 +0530

    Added Operators in python

---
+++ 6.python_operator.py
@@ -0,0 +1,41 @@
+a=32
+b=5
+
+#Arithmetic Operators
+
+print("a+b=",a+b,)
+print("a-b=",a-b,)
+print("a*b=",a*b,)
+print("a/b=",a/b,)
+print("a%b=",a%b,)
+print("a//b=",a//b,)
+
+#conditional operators  
+
+print(a>4)
+print(a<4)
+print(a==4)
+print(a!=4)
+print(a>=4)
+print(a<=4)
+
+c=True
+d=False
+#logical operators
+print(c and d)
+print(c or d)
+print(not c)    
+print(d and d)
+print(d or d)
+print(not d)
+
+
+#assignment operators
+a+=5
+print(a)
+a-=5
+print(a)
+a*=5
+print(a)
+a/=5
+print(a)    
