73136591426742c69401870bdc56e906eda754fc
gawchar33 <gawchar33@gmail.com>
2026-03-07 23:24:08 +0530

    Added function_concept folder

---
+++ function_concept/funargs_returns.py
@@ -0,0 +1,23 @@
+#type of arguments in python 
+#1.Positional argument
+#2.Default arguments
+#3.Keyword arguements
+
+#1.positional argument
+
+def add(a,b):
+    return a+b
+print(add(3,4))
+
+#2.default arguments
+
+def greeting(name="Friends"):
+    return f"Hell0 {name}"# if u seperate this with , python will create the tuple here 
+greet=greeting()
+print(greet)
+
+#keyword arguments
+def add(a,b,c=10):
+    return f"the sum of a,b and the default argument c is {a+b+c}"
+sum=add(b=1,a=4,c=0)
+print(sum)
---
+++ function_concept/lambdafunc.py
@@ -0,0 +1,5 @@
+'''lambda function is the short function
+   syntax of the lambda function is  lambda parameters:expressions '''
+
+add=lambda a,b:a + b
+print(add(1,2))
